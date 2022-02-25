971ea22e2df42122df4788b74b46c6896db1a13b
aguzbruno <aguzbruno@hotmail.com>
2022-02-25 11:07:31 -0300

    changes css

--- Desktop/Programacion/WORDLE/wordle/src/components/Home/Home.jsx
+++ Desktop/Programacion/WORDLE/wordle/src/components/Home/Home.jsx
@@ -3,8 +3,9 @@ export default function Home(){
     return <div className="Home">
         <div className="Line">
         <div className="Wordle">
-        WORDLE (ES)
+        WORDLE
     </div>
+    UNLIMITED
     </div>
     <a style={{color:"#c9b458"}} href="https://github.com/aguzbruno">by Aguzbruno</a>
     </div>
